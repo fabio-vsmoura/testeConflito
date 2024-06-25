40dd07478aa1e7351f741c47082e0328ee1f4771
fabio-vsmoura <fb.vsmoura@gmail.com>
2024-06-25 16:55:20 -0300

    Mensagem do junior

--- testeConflito/testeConflito/Form1.cs
+++ testeConflito/testeConflito/Form1.cs
@@ -9,7 +9,9 @@ namespace testeConflito
 
         private void button1_Click(object sender, EventArgs e)
         {
+            string texto = "Lendy";
             MessageBox.Show("Bem-vindo ao GIT HUB");
+            MessageBox.Show("E ai " + texto);
         }
     }
 }
